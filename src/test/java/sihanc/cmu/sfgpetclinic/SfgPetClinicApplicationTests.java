0334c9dd645d791a61d58838fc47c48c334efcd1
coderballer <shhchen122@gmail.com>
2022-04-19 12:55:39 -0400

    Initial Project Add

---
+++ src/test/java/sihanc/cmu/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package sihanc.cmu.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

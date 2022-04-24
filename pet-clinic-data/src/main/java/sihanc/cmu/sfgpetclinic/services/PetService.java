ce89f122220a5d40a070d61c3cf69c8110dc5079
coderballer <shhchen122@gmail.com>
2022-04-24 17:12:10 -0400

    refactor to common crud interface. closes #11

--- pet-clinic-data/src/main/java/sihanc/cmu/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/sihanc/cmu/sfgpetclinic/services/PetService.java
@@ -1,15 +1,7 @@
 package sihanc.cmu.sfgpetclinic.services;
 
-import sihanc.cmu.sfgpetclinic.model.Owner;
 import sihanc.cmu.sfgpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/sihanc/cmu/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/sihanc/cmu/sfgpetclinic/services/VetService.java
@@ -1,16 +1,7 @@
 package sihanc.cmu.sfgpetclinic.services;
 
-import sihanc.cmu.sfgpetclinic.model.Owner;
 import sihanc.cmu.sfgpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 
 }
